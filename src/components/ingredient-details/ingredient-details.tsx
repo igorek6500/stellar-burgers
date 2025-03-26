993c5567bfa4ae5df29289cb07ca10f2186bb4cc
Sphinx Code <igorek6500@yandex.ru>
2025-03-26 13:56:43 +0300

    Добавлен функционал для работы с сайтом

--- src/components/ingredient-details/ingredient-details.tsx
+++ src/components/ingredient-details/ingredient-details.tsx
@@ -1,10 +1,17 @@
+import { getIngredientsState } from '../../services/slices/Ingredients';
 import { FC } from 'react';
-import { Preloader } from '../ui/preloader';
-import { IngredientDetailsUI } from '../ui/ingredient-details';
+import { Preloader } from '@ui';
+import { IngredientDetailsUI } from '@ui';
+import { useSelector } from '../../services/store';
+import { useParams } from 'react-router-dom';
 
 export const IngredientDetails: FC = () => {
-  /** TODO: взять переменную из стора */
-  const ingredientData = null;
+  const { ingredients } = useSelector(getIngredientsState);
+  const { id } = useParams();
+
+  const ingredientData = ingredients.find(
+    (ingredient) => ingredient._id === id
+  );
 
   if (!ingredientData) {
     return <Preloader />;
